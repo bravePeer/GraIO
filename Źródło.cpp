c4e9c060c99765aad53180d8ad4413f808d2e62a
Bartłomiej Gojowczyk <bartek.gojowczyk01@gmail.com>
2021-12-07 13:48:09 +0100

    Skonfigurowany projet z SFML

--- Źródło.cpp
+++ Źródło.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
+using namespace sf;
 
 int main()
 {
-	cout << "som update" << endl;
+	RenderWindow window(VideoMode(1600, 900, 64), "Gra IO");
+
+	Event events;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(events))
+		{
+			if (events.type == Event::Closed)
+				window.close();
+		}
+
+
+	}
 
 	return 0;
 }
